63e589db78ceaf97b7a40b2128f28bd7528f4c01
opilane <opilane@HP-CZC2349HTF>
2023-01-18 16:16:12 +0200

    Add project files.

---
+++ Контрольная работа по теме Циклы/Контрольная_работа_по_теме_Циклы.py
@@ -0,0 +1,62 @@
+from random import *
+from math import *
+from re import U
+
+3
+print("1")
+print()
+
+try:
+    n=int(input("vali 1-9 palju tahad näha kuuske? "))
+    if n<1 or n>9:
+        print("vale vahemik")
+except:
+    print("vale andmetüüp")
+x=0
+for x in range(4):
+    for e in range(n):
+        if x==0:
+            print(" ----- ", end="")
+        elif x ==1:
+            print("|  O  |", end="")
+        elif x==2:
+            print("!  -  !", end="")
+        elif x==3:
+            print(" ----- ", end="")
+    print("")
+
+
+print()
+print("2")
+print()
+
+nch=int(input("vvedite chislo> "))
+st=int(input("v kakoi steoeni eto chislo> "))
+nch1 =stn= 1
+while stn<nch:
+    stn=nch1**st
+    if stn>nch:
+        break
+    print(nch1,"^",st,"=",stn)
+    nch1 +=1
+
+print()
+print("3")
+print()
+
+print("otsenki po fizike")
+u=randint(1,30)
+nomer=1
+suma=0
+for i in range (u):
+    o=randint(2,5)
+    print(nomer,"->",o)
+    nomer +=1
+    suma +=o
+nome=nomer-1
+kesk=suma/nome
+print("keskmine on:", kesk)
+
+print()
+print("4")
+print()
